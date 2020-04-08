f40335b0191c57b5778e3a2e93272ede92a3557a
mike <fj110808@163.com>
2020-04-08 10:54:28 +0800

    fix:

---
+++ template/reactComponent/index.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import {useIntl} from 'react-intl';
+import Style from './index.module.scss'
+
+const Component = (props: any) => {
+  const intl = useIntl()
+  return (
+    <>
+    </>
+  )
+}
+export default Component;
